7f0e29c1740b13d63c618465b18e4c0449a0c214
Leandro Iamele <leandroiamele@gmail.com>
2020-09-08 10:53:16 -0300

    Add meaterialize css and add icons to index

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -19,13 +19,11 @@ class FlatsController < ApplicationController
 
   def create
     @flat = Flat.new(flat_params)
-
     if @flat.save
       redirect_to @flat 
     else
       render 'new'
     end
-
   end
 
   def edit
